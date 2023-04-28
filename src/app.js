eb83e5c66dd609153c55bf1ea7748798e5ea1d9b
Carlos <41452910+usarral@users.noreply.github.com>
2023-04-28 12:27:59 +0200

    :wrench: Add express.json for read post json data

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import userRoutes from './routes/user.routes.js'
 import generalRoutes from './routes/general.routes.js'
 
 const app = express()
+app.use(express.json())
 
 app.use(morgan(TYPE))
 app.use('/arbitro', arbitroRoutes)
