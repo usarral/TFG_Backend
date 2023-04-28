f73198fe4cfe7a842b48d8fabeb986c83c3d20da
Carlos <41452910+usarral@users.noreply.github.com>
2023-04-28 11:35:24 +0200

    :wrench: Rename .env vars in config file

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const PORT = process.env.SERVER_PORT || 3000
 const TYPE = process.env.SERVER_ENV || 'dev'
 const MONGO_URI =
   process.env.MONGO_URI ||
-  `mongodb://${process.env.MONGO_USERNAME || 'root'}:${
-    process.env.MONGO_PASSWORD || 'root'
+  `mongodb://${process.env.MONGO_ROOT_USERNAME || 'root'}:${
+    process.env.MONGO_ROOT_PASSWORD || 'root'
   }@${process.env.MONGO_HOST || 'localhost'}:${process.env.MONGO_PORT || 27017}`
 export { PORT, TYPE, MONGO_URI }
