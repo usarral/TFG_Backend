d3ca8fc89b58b5086ff1478322aa0168a6d97940
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-01 17:01:16 +0200

    :wrench: Update baseURL config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ dotenv.config()
 const PROTOCOL = process.env.SERVER_PROTOCOL || 'http'
 const HOST = process.env.SERVER_HOST || 'localhost'
 const PORT = process.env.SERVER_PORT || 3000
-const BASEURL = process.env.BASEURL || `http://${HOST}:${PORT}`
+const BASEURL = process.env.BASEURL || `${PROTOCOL}://${HOST}:${PORT}`
 const TYPE = process.env.SERVER_ENV || 'dev'
 const MONGO_URI =
   process.env.MONGO_URI ||
@@ -12,4 +12,4 @@ const MONGO_URI =
   }@${process.env.MONGO_HOST || 'localhost'}:${
     process.env.MONGO_PORT || 27017
   }/${process.env.MONGO_DATABASE || 'app'}?authSource=admin`
-export { PORT, TYPE, MONGO_URI }
+export { PORT, TYPE, MONGO_URI, BASEURL, PROTOCOL, HOST }
