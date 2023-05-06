822b775058c2a55c676bbf6791819dd513c86424
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-06 13:35:35 +0200

    :white_check_mark: Add structure for test

---
+++ src/test/categoria.test.js
@@ -0,0 +1,13 @@
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+const fetch = require('node-fetch')
+const { BASEURL } = require('../config.js')
+let createdId
+
+describe('Tests de API de Categoria', () => {
+  test('Test Crear Categoria', async () => {})
+  test('Test Obtener Categorias', async () => {})
+  test('Test Obtener Categoria por ID', async () => {})
+  test('Test Modificar Categoria por ID', async () => {})
+  test('Test Eliminar Categoria por ID', async () => {})
+})
