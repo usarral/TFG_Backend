b6ba76655b3191064825252b19730968becef9bc
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-06 13:36:43 +0200

    :white_check_mark: Add test structure

---
+++ src/test/arbitro.test.js
@@ -0,0 +1,13 @@
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+const fetch = require('node-fetch')
+const { BASEURL } = require('../config.js')
+let createdId
+
+describe('Tests de API de Arbitro', () => {
+  test('Test Crear Arbitro', async () => {})
+  test('Test Obtener Arbitros', async () => {})
+  test('Test Obtener Arbitro por ID', async () => {})
+  test('Test Modificar Arbitro por ID', async () => {})
+  test('Test Eliminar Arbitro por ID', async () => {})
+})
