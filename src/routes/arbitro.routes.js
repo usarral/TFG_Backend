e202341ec558c2f8bd3b59729dcb49b7e8689884
Carlos <41452910+usarral@users.noreply.github.com>
2023-05-06 10:43:55 +0200

    :building_construction: getArbitros route

--- src/routes/arbitro.routes.js
+++ src/routes/arbitro.routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
 const router = Router()
+
+router.get('/', getArbitros)
+
 // 404 error
 router.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' })
