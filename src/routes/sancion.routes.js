17ab360e4071016006cb3d5bd6b9d39d15d20036
Carlos <41452910+usarral@users.noreply.github.com>
2023-05-08 11:14:33 +0200

    :sparkles: Add routes for new features

--- src/routes/sancion.routes.js
+++ src/routes/sancion.routes.js
@@ -1,5 +1,21 @@
 import { Router } from 'express'
 const router = Router()
+import {
+  createSancion,
+  getSanciones,
+  updateSancion,
+  deleteSancion,
+  getSancionById
+} from '../controllers/sancion.controller.js'
+
+// /api/sancion
+router.post('/', createSancion)
+router.get('/', getSanciones)
+
+// /api/sancion/:id
+router.get('/:id', getSancionById)
+router.put('/:id', updateSancion)
+router.delete('/:id', deleteSancion)
 
 // 404 error
 router.use((req, res) => {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,23 @@
 import { Router } from 'express'
 const router = Router()
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  deleteUser,
+  updateUser
+} from '../controllers/user.controller.js'
+
+// GET /users
+router.get('/', getUsers)
+// GET /users/:id
+router.get('/:id', getUserById)
+// POST /users
+router.post('/', createUser)
+// PUT /users/:id
+router.put('/:id', updateUser)
+// DELETE /users/:id
+router.delete('/:id', deleteUser)
 
 // 404 error
 router.use((req, res) => {
