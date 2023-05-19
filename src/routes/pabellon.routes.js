388592ca2918b011e3644bc60d9774d32e1a8ff3
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-19 12:18:47 +0200

    :sparkles: Pabellon routes

--- src/routes/pabellon.routes.js
+++ src/routes/pabellon.routes.js
@@ -1,6 +1,18 @@
 import { Router } from 'express'
 const router = Router()
+import {
+  createPabellon,
+  getPabellones,
+  getPabellon,
+  deletePabellon,
+  updatePabellon
+} from '../controllers/pabellon.controller.js'
 
+router.get('/', getPabellones)
+router.post('/', createPabellon)
+router.get('/:id', getPabellon)
+router.delete('/:id', deletePabellon)
+router.put('/:id', updatePabellon)
 // 404 error
 router.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' })
