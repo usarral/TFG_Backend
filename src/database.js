617ff14b10fb21288bf5c0ae56d00cc4027a194d
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-21 17:32:35 +0200

    :goal_net: Catch db conn errors and reconnect

--- src/database.js
+++ src/database.js
@@ -1,7 +1,24 @@
 import mongoose from 'mongoose'
 import { MONGO_URI } from './config.js'
 
-mongoose
-  .connect(MONGO_URI)
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.log(err))
+const connectWithRetry = () => {
+  console.log('Conectando a la base de datos...')
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => console.info('Conexión a la base de datos establecida'))
+    .catch(error => {
+      console.error('Error al conectar a la base de datos:', error)
+      setTimeout(connectWithRetry, 5000)
+    })
+}
+
+connectWithRetry()
+
+setInterval(() => {
+  if (mongoose.connection.readyState === 0) {
+    console.warn(
+      'Conexión a la base de datos perdida, intentando reconectar...'
+    )
+    connectWithRetry()
+  }
+}, 10000)
