914b58c9d89c263f543c9e6481eb50177c1fe4c3
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-30 09:08:12 +0200

    :sparkles: Partido route

--- src/routes/partido.routes.js
+++ src/routes/partido.routes.js
@@ -1,6 +1,29 @@
 import { Router } from 'express'
 const router = Router()
 
+import {
+  createPartido,
+  getPartidos,
+  getPartido,
+  updatePartido,
+  deletePartido
+} from '../controllers/partido.controller.js'
+
+// GET: Return all partidos
+router.get('/', getPartidos)
+
+// GET: Return a partido by id
+router.get('/:id', getPartido)
+
+// POST: Create a new partido
+router.post('/', createPartido)
+
+// PUT: Update partido by id
+router.put('/:id', updatePartido)
+
+// DELETE: Delete partido by id
+router.delete('/:id', deletePartido)
+
 // 404 error
 router.use((req, res) => {
   res.status(404).json({ message: 'Endpoint not found' })
