f16467bb5903c7aefbbae62f82c1802510185bf3
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-05-30 09:07:50 +0200

    :sparkles: Login route

--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import { login } from '../controllers/user.controller.js'
 const router = Router()
 
 router.get('/', (req, res) => {
   res.json({ message: 'Welcome to the API' })
 })
+router.post('/login', login)
 
 // 404 error
 router.use((req, res) => {
